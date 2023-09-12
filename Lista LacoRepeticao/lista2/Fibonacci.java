f645129de578d346713826a44f02e9d8f1f07176
Camila <142915203+CamilaNefertite@users.noreply.github.com>
2023-09-12 19:53:56 -0300

    Update Fibonacci.java

--- Lista LacoRepeticao/lista2/Fibonacci.java
+++ Lista LacoRepeticao/lista2/Fibonacci.java
@@ -3,22 +3,19 @@ public class Fibonacci {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int nAntes=0, nAtual=1, i=2;
-        int nDepois, compi;
+        int nDepois, posicao;
         
         System.out.println("Digite a posição desejada:");
-        compi=in.nextInt();
+        posicao=in.nextInt();
         
         
-        while (i<=compi){
+        while (i<=posicao){
             nDepois=nAtual+nAntes;
             nAntes=nAtual;
             nAtual=nDepois;
             
              System.out.println(nDepois);
             i++;
-            
-        }
-        
+            }
     }
-    
 }
