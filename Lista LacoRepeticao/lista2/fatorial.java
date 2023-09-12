1fdcdd97ea72b1bfd2b74d68a826ff602f9af834
Camila <142915203+CamilaNefertite@users.noreply.github.com>
2023-09-12 19:50:09 -0300

    Update fatorial.java

--- Lista LacoRepeticao/lista2/fatorial.java
+++ Lista LacoRepeticao/lista2/fatorial.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-	public class fatorial {
+public class fatorial {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int i=1, multi=1, numero;
