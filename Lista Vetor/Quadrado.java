5dac75816ad010e8e0cb3181597dba4028982eb6
Camila <142915203+CamilaNefertite@users.noreply.github.com>
2023-11-21 02:07:31 -0300

    Add files via upload

---
+++ Lista Vetor/Quadrado.java
@@ -0,0 +1,37 @@
+package vetor;
+import java.util.Scanner;
+
+public class Quadrado {
+	public static void main(String[] args) {
+		
+		Scanner in = new Scanner(System.in);
+
+		int i;
+		int[] a, b;
+		final int TAM = 10;
+		
+		a = new int[TAM];
+		b = new int[TAM];
+		
+		for (i = 0; i < TAM; i++) {
+			System.out.println("Diite o " + (i + 1) + "º valor: ");
+			a[i] = in.nextInt();
+		}
+		
+		System.out.print("\nA = [ ");
+		for (i = 0; i < TAM; i++) {
+			System.out.print(a[i] + " ");
+		}
+		System.out.print(" ]");
+		
+		System.out.print("\nB = [ ");
+		for (i = 0; i < TAM; i++) {
+			b[i] = a[i] * a[i];
+			System.out.print(b[i] + " ");
+		}
+		System.out.print("]");
+
+		in.close();
+		System.exit(0);
+	}
+}
