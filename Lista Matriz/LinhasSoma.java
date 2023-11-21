ab7c981b80d4401b77645c6a29df6dc6e652d493
Camila <142915203+CamilaNefertite@users.noreply.github.com>
2023-11-21 02:05:57 -0300

    Add files via upload

---
+++ Lista Matriz/LinhasSoma.java
@@ -0,0 +1,27 @@
+package matriz;
+import java.util.Scanner;
+public class LinhasSoma{
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int matriz[][] = new int[3][4];
+        int i, j, soma=0;
+
+        for (i = 0; i < 3; i++) {
+            for (j = 0; j < 3; j++) {
+                System.out.println("Digite os números:");
+                matriz[i][j] = in.nextInt();
+            }
+        }
+        for (i = 0; i < 3; i++) {
+            for (j = 0; j < 3; j++) {
+                soma += matriz[i][j];
+            }matriz[i][3] = soma;
+        }
+        for (i = 0; i < 3; i++) {
+            for (j = 0; j < 4; j++) {
+                System.out.print(matriz[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
